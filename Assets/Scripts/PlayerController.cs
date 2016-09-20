bbfae6621e9d4ce2249eb0abfbcb013718326484
CKiilu <cndeti@gmail.com>
2016-09-20 19:36:12 +0300

    2D movement done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb2D;
+    public float speed;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+    
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical= Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2D.AddForce(movement * speed);
+    }
+}
